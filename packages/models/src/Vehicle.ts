ce79082aeda07fe898d56e0da57a856aff9663d1
Matheus Santos <matheus.santos@caronsale.de>
2022-10-30 16:19:43 -0300

    feat: add license plate

--- packages/models/src/Vehicle.ts
+++ packages/models/src/Vehicle.ts
@@ -1,6 +1,7 @@
 export class Vehicle {
     public id: string;
     public vin: string;
+    public licensePlate: string;
 
     public toString(): string {
         return `id: ${this.id} | vin: ${this.vin}`;
