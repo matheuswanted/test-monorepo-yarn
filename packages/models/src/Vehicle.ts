2621e1ea9fe5e177b7a3725ee1d9c929ba53746b
Matheus Santos <matheus.santos@caronsale.de>
2022-10-31 16:17:22 -0300

    feat: add test field

--- packages/models/src/Vehicle.ts
+++ packages/models/src/Vehicle.ts
@@ -2,6 +2,7 @@ export class Vehicle {
     public id: string;
     public vin: string;
     public licensePlate: string;
+    public testField: string;
 
     public toString(): string {
         return `id: ${this.id} | vin: ${this.vin}`;
